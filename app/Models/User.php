125b49908826c5709ef09e833d509df47de324e7
Star <gzstar117@aliyun.com>
2018-07-29 13:29:50 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    //与Topic的关联模型
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
