03edb568f83e1e24a2a76391ad4128566533eda0
Star <gzstar117@aliyun.com>
2018-08-04 12:58:19 +0000

    Users can edit their own topic

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,10 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //给Topic的内容做XSS防御, XSS的过滤配置在文件 config\purifier.php
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        //给Topic生成摘要
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
